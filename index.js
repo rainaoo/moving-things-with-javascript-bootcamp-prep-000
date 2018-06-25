2d51935b226f59333183e873ebe6460d96df434f
raina <rainaoo2017@gmail.com>
2018-06-25 12:29:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function moveDodgerRight() {
     dodger.style.left = `${left + 1}px`
   }
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerRight()
+  }
+})
